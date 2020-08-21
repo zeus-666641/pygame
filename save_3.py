63fc81d2f29a814b6a1e080b5ce118d0c3356856
宙斯 <63239083+zeus-666641@users.noreply.github.com>
2020-08-21 19:05:07 +0800

    Create save_3.py

---
+++ save_3.py
@@ -0,0 +1,109 @@
+#coding:cp936
+name = '857'
+fps = 64
+tips = True
+sound = True
+sound_4 = True
+coins = 0
+player_speed_3 = False
+hp_2 = False
+player_speed = 3
+hp = 3
+die_number = 0
+e = 0
+open_level = 0
+level_ok_number = 0
+first_open_game = True
+save_name_true = True
+
+first_cheng_jiu_open = False
+first_shop_open = False
+first_upgrade_open = False
+first_tong_ji_open = False
+first_upgrade_dao_ju_open = False
+first_dui_huan_open = False
+
+first_enter_cheng_jiu = False
+first_enter_shop = False
+first_enter_upgrade = False
+first_enter_tong_ji = False
+first_enter_dui_huan = False
+
+cheng_jiu_open = False
+shop_open = False
+upgrade_open = False
+tong_ji_open = False
+upgrade_dao_ju_open = False
+dui_huan_open = False
+
+su_du_upgrade_number = 0
+hp_upgrade_number = 0
+upgrade_number = 0
+
+dao_ju_hp_level = 1
+dao_ju_su_du_level = 1
+dao_ju_lower_level = 1
+dao_ju_coins_two_level = 1
+
+hui_fu_number = 0
+su_du_number = 0
+lower_number = 0
+coins_two_number = 0
+
+starts = 0
+starts_open_1 = False
+starts_open_2 = False
+starts_open_3 = False
+starts_open_4 = False
+starts_open_5 = False
+
+cheng_jiu_cishu_1 = False
+cheng_jiu_cishu_2 = False
+cheng_jiu_cishu_3 = False
+cheng_jiu_cishu_4 = False
+cheng_jiu_cishu_5 = False
+cheng_jiu_cishu_6 = False
+cheng_jiu_cishu_7 = False
+cheng_jiu_cishu_8 = False
+cheng_jiu_cishu_9 = False
+cheng_jiu_cishu_10 = False
+cheng_jiu_cishu_11 = False
+cheng_jiu_cishu_12 = False
+cheng_jiu_cishu_13 = False
+cheng_jiu_cishu_14 = False
+
+coins_dui_huan_1 = False
+coins_dui_huan_2 = False
+coins_dui_huan_3 = False
+coins_dui_huan_4 = False
+coins_dui_huan_5 = False
+coins_dui_huan_6 = False
+coins_dui_huan_7 = False
+coins_dui_huan_8 = False
+coins_dui_huan_9 = False
+coins_dui_huan_10 = False
+coins_dui_huan_11 = False
+
+first_level_1 = False
+first_level_2 = False
+first_level_3 = False
+first_level_4 = False
+first_level_5 = False
+
+tong_guan_1 = False
+tong_guan_2 = False
+tong_guan_3 = False
+tong_guan_4 = False
+tong_guan_5 = False
+
+donut_open = 1
+level_1_donut = False
+level_2_donut = False
+level_3_donut = False
+level_4_donut = False
+level_5_donut = False
+
+first_buy = 1
+
+shop_newer_tips = 1
+dui_huan_newer_tips = False
